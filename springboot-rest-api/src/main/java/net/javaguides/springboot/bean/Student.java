b54c8af332e62f7c12ef6fca85d9af28c38c9fb1
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2024-10-06 23:59:38 +0530

    9. Spring Boot REST API returns Java Bean

---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/bean/Student.java
@@ -0,0 +1,37 @@
+package net.javaguides.springboot.bean;
+
+public class Student {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public Student(int age, String firstName, String lastName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -0,0 +1,22 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    // http://localhost:8080/student
+    @GetMapping("student")
+    //returns object
+    public Student getStudent(){
+        Student student = new Student(
+                27,
+                "Tom",
+                "Holland"
+        );
+
+        return student;
+    }
+}
